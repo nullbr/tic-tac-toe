cbc8f8caf403a9986cc46adaf9a94d96071795f2
Bruno Leite <bmarianoleite3@gmail.com>
2023-05-17 15:04:11 -0300

    setting symbol

--- lib/tic_tac_toe/game.rb
+++ lib/tic_tac_toe/game.rb
@@ -7,9 +7,9 @@ class Game
 
   # Determine the possible types of game finish
   FINISH_TYPES = {
-    "diagonal": [[0, 4, 8], [2, 4, 6]],
-    "vertical": [[0, 3, 6], [1, 4, 7], [2, 5, 8]],
-    "horizontal": [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+    diagonal: [[0, 4, 8], [2, 4, 6]],
+    vertical: [[0, 3, 6], [1, 4, 7], [2, 5, 8]],
+    horizontal: [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
   }.freeze
 
   def initialize(player1 = "", player2 = "")
